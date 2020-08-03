4d73917e3466e3a3681887ed3068e18015e37f62
Thiago <thi_meneghetti@hotmail.com>
2020-08-03 18:44:16 -0300

    formCategoria

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../componentes/PageDefault';
+import FormField from '../../../componentes/FormField';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria(){
+   const [categorias, setCategorias] = useState([]);
+   const valoresIniciais = {
+      nome: '',
+      descricao: '',
+      cor: '',
+   };
+   const [valores, setValores] = useState(valoresIniciais);
+
+   function setValor(chave, valor){
+      setValores({
+        ...valores,
+        [chave]: valor,
+      })
+   }
+
+   function mudarImput(infoEvento){               
+    setValor(infoEvento.target.getAttribute('name'), 
+    infoEvento.target.value);
+  }
+
+   console.log('[nomeDaCategoria]' , valores);
+
     return(
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {valores.nome}</h1>
 
-        <form>
-          <label>Nome da Categoria</label>
-          <input type="text"></input>
+        <form onSubmit = {function handleSubmit(infoEvento){
+                          infoEvento.preventDefault(); 
+                          setCategorias([ ...categorias, valores ]);
+                          console.log('Formulario Enviado');
+                          setValores(valoresIniciais);
+                        }}>
+          <FormField 
+            label= "Nome da Categoria:"
+            type= "text"
+            name= "nome"
+            value= {valores.nome}
+            onChange= {mudarImput}
+          />
+          <div>
+            <label>Descrição:</label>
+            <textarea type="text" name= "descricao" value= {valores.descricao}  
+              onChange= {mudarImput}>
+            </textarea>
+          </div>
+          {/*
+          <div>
+            <label>Cor:</label>
+            <input type="color" name= "cor" value= {valores.cor}  
+              onChange= {mudarImput}>
+            </input>
+          </div>   */}
+          <FormField 
+            label= "Cor:"
+            type= "color"
+            name= "cor"
+            value= {valores.cor}
+            onChange= {mudarImput}
+          />
           <button>Cadastrar </button>
         </form>
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key = {categoria + indice}>
+                {categoria.nome}
+              </li>
+            )
+          })}
+        </ul>
         
         <Link to="/"> 
             Ir pra home

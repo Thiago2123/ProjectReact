7dedf9745721c1b6d5e2602634c259de3572fa8b
Thiago <thi_meneghetti@hotmail.com>
2020-08-04 19:35:52 -0300

    aa

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -1,85 +1,110 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../componentes/PageDefault';
 import FormField from '../../../componentes/FormField';
-import { Link } from 'react-router-dom';
 
-function CadastroCategoria(){
-   const [categorias, setCategorias] = useState([]);
-   const valoresIniciais = {
-      nome: '',
-      descricao: '',
-      cor: '',
-   };
-   const [valores, setValores] = useState(valoresIniciais);
+function CadastroCategoria() {
+  const [categorias, setCategorias] = useState([]);
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [valores, setValores] = useState(valoresIniciais);
 
-   function setValor(chave, valor){
-      setValores({
-        ...valores,
-        [chave]: valor,
-      })
-   }
-
-   function mudarImput(infoEvento){               
-    setValor(infoEvento.target.getAttribute('name'), 
-    infoEvento.target.value);
+  function setValor(chave, valor) {
+    setValores({
+      ...valores,
+      [chave]: valor,
+    });
   }
 
-   console.log('[nomeDaCategoria]' , valores);
-
-    return(
-      <PageDefault>
-        <h1>Cadastro de Categoria: {valores.nome}</h1>
-
-        <form onSubmit = {function handleSubmit(infoEvento){
-                          infoEvento.preventDefault(); 
-                          setCategorias([ ...categorias, valores ]);
-                          console.log('Formulario Enviado');
-                          setValores(valoresIniciais);
-                        }}>
-          <FormField 
-            label= "Nome da Categoria:"
-            type= "text"
-            name= "nome"
-            value= {valores.nome}
-            onChange= {mudarImput}
-          />
-          <div>
-            <label>Descrição:</label>
-            <textarea type="text" name= "descricao" value= {valores.descricao}  
-              onChange= {mudarImput}>
-            </textarea>
-          </div>
-          {/*
-          <div>
-            <label>Cor:</label>
-            <input type="color" name= "cor" value= {valores.cor}  
-              onChange= {mudarImput}>
-            </input>
-          </div>   */}
-          <FormField 
-            label= "Cor:"
-            type= "color"
-            name= "cor"
-            value= {valores.cor}
-            onChange= {mudarImput}
-          />
-          <button>Cadastrar </button>
-        </form>
-        <ul>
-          {categorias.map((categoria, indice) => {
-            return (
-              <li key = {categoria + indice}>
-                {categoria.nome}
-              </li>
-            )
-          })}
-        </ul>
-        
-        <Link to="/"> 
-            Ir pra home
-        </Link>
-      </PageDefault>
-    )
+  function mudarImput(infoEvento) {
+    setValor(infoEvento.target.getAttribute('name'),
+      infoEvento.target.value);
   }
 
-  export default CadastroCategoria;
+  useEffect(() => {
+    console.log('aq');
+    const URL = 'urlAqui';
+    fetch(URL)
+      .then(async(respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([...resposta]);
+      });
+
+    /*setTimeout(() => {
+      setCategorias([...categorias, {   
+        "id" : 1,
+        "nome" : "Front End",
+        "descricao" : "Uma categoria pica",
+        "cor" : "#cdb1ff"
+    },
+    {   
+        "id" : 2,
+        "nome" : "Back End",
+        "descricao" : "Uma categoria pica",
+        "cor" : "#cdb1ff"
+    }]);
+    }, 3 * 1000);*/
+  }, [
+
+  ]);
+
+  //console.log('[nomeDaCategoria]', valores);
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {valores.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infoEvento) {
+        infoEvento.preventDefault();
+        setCategorias([...categorias, valores]);
+        console.log('Formulario Enviado');
+        setValores(valoresIniciais);
+      }}
+      >
+        <FormField
+          label="Nome da Categoria:"
+          type="text"
+          name="nome"
+          value={valores.nome}
+          onChange={mudarImput}
+        />
+        <FormField
+          label="Descrição:"
+          type="textarea"
+          name="descricao"
+          value={valores.descricao}
+          onChange={mudarImput}
+        />
+        <FormField
+          label="Cor:"
+          type="color"
+          name="cor"
+          value={valores.cor}
+          onChange={mudarImput}
+        />
+        <button>Cadastrar </button>
+        {categorias.length === 0 && 
+          <div>Loading...</div>
+        }
+      </form>
+      <ul>
+        {categorias.map((categoria, indice) => (
+          <li key={categoria + indice}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir pra home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
